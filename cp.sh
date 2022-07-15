eccad15eca8087454780db40dce5dea1676a2fec
qsyyke <2291308094@qq.com>
2022-07-15 09:43:47 +0800

    init

---
+++ cp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm -rf /opt/aurora-theme/run-jar/jar/*
+
+\cp -r /opt/aurora-theme/file/aurora-blog/aurora-modules/admin-boot/target/admin-boot-1.0-SNAPSHOT.jar /opt/aurora-theme/run-jar/jar/
+\cp -r /opt/aurora-theme/file/aurora-blog/aurora-modules/aurora-gateway/target/aurora-gateway-1.0-SNAPSHOT.jar /opt/aurora-theme/run-jar/jar/
+
+\cp -r  /opt/aurora-theme/file/aurora-blog/aurora-modules/file-boot/target/file-boot-1.0-SNAPSHOT.jar /opt/aurora-theme/run-jar/jar/
+
+\cp -r /opt/aurora-theme/file/aurora-blog/aurora-modules/oauth-auth-server/target/oauth-auth-server-1.0-SNAPSHOT.jar /opt/aurora-theme/run-jar/jar/
+
+\cp -r  /opt/aurora-theme/file/aurora-blog/aurora-modules/aurora-article/target/aurora-article-1.0-SNAPSHOT.jar /opt/aurora-theme/run-jar/jar/
+
+\cp -r /opt/aurora-theme/file/aurora-blog/aurora-modules/comment-boot/target/comment-boot-1.0-SNAPSHOT.jar /opt/aurora-theme/run-jar/jar/
+
+\cp -r  /opt/aurora-theme/file/aurora-blog/aurora-modules/message-boot/target/message-boot-1.0-SNAPSHOT.jar /opt/aurora-theme/run-jar/jar/
---
+++ runjar.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+java -jar /opt/aurora-theme/run-jar/jar/admin-boot-1.0-SNAPSHOT.jar
+java -jar /opt/aurora-theme/run-jar/jar/aurora-gateway-1.0-SNAPSHOT.jar
+java -jar /opt/aurora-theme/run-jar/jar/file-boot-1.0-SNAPSHOT.jar
+java -jar /opt/aurora-theme/run-jar/jar/oauth-auth-server-1.0-SNAPSHOT.jar
+java -jar /opt/aurora-theme/run-jar/jar/aurora-article-1.0-SNAPSHOT.jar
+java -jar /opt/aurora-theme/run-jar/jar/comment-boot-1.0-SNAPSHOT.jar
+java -jar /opt/aurora-theme/run-jar/jar/message-boot-1.0-SNAPSHOT.jar
